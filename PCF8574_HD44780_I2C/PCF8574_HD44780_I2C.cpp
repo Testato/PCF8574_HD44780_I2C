1d232822cb3f9ceb589ab78593d869c26eb2ba02
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-05-25 08:16:33 +0200

    Update PCF8574_HD44780_I2C.cpp

--- PCF8574_HD44780_I2C/PCF8574_HD44780_I2C.cpp
+++ PCF8574_HD44780_I2C/PCF8574_HD44780_I2C.cpp
@@ -42,13 +42,15 @@ void PCF8574_HD44780_I2C::init() {
 	init_priv();
 }
 
-void PCF8574_HD44780_I2C::init_priv() {
+void PCF8574_HD44780_I2C::init_priv() 
+{
 	Wire.begin();
 	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
 	begin(_cols, _rows);
 }
 
-void PCF8574_HD44780_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
+void PCF8574_HD44780_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) 
+{
 	if (lines > 1) {
 		_displayfunction |= LCD_2LINE;
 	}
@@ -108,8 +110,8 @@ void PCF8574_HD44780_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
 
 /********** high level commands, for the user! */
 void PCF8574_HD44780_I2C::clear() {
-	command(LCD_CLEARDISPLAY);   // clear display, set cursor position to zero
-	delayMicroseconds(2000);	 // this command takes a long time!
+	command(LCD_CLEARDISPLAY); // clear display, set cursor position to zero
+	delayMicroseconds(2000);   // this command takes a long time!
 }
 
 void PCF8574_HD44780_I2C::home() {
@@ -119,7 +121,7 @@ void PCF8574_HD44780_I2C::home() {
 
 void PCF8574_HD44780_I2C::setCursor(uint8_t col, uint8_t row) {
 	int row_offsets[] = {0x00, 0x40, 0x14, 0x54}; // LCD Addressing
-	if (row > _numlines) 	{
+	if (row > _numlines) {
 		row = _numlines - 1; // we count rows starting w/0
 	}
 	command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
